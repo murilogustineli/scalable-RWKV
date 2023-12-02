9c64eddc41fbd4b37aca6780c7705bd0e70afc68
Rob Brock <rbrock8@gatech.edu>
2023-12-02 03:34:49 -0500

    fixed txt2jsonl parsing and eot issue

--- RWKV-v4neo/txt2jsonl.py
+++ RWKV-v4neo/txt2jsonl.py
@@ -1,13 +1,31 @@
 import json
 
-# Replace 'input.txt' with your text file's name
-with open('data/TinyStories-valid.txt', 'r') as txt_file:
-    # Replace 'output.jsonl' with your desired output file's name
-    with open('data/TinyStories-valid.jsonl', 'w') as jsonl_file:
-        for line in txt_file:
-            # Process the line into a JSON object
-            # This example assumes each line is a simple text entry
-            json_object = {"text": line.strip()}
+def split_stories(text):
+    # Splitting stories based on the specific separator line
+    separator = '<|endoftext|>'
+    return text.split(separator)
 
-            # Write the JSON object to the .jsonl file
-            jsonl_file.write(json.dumps(json_object) + '\n')
+def convert_to_jsonl(stories, output_file):
+    with open(output_file, 'w') as file:
+        for story in stories:
+            # Cleaning and trimming each story
+            story_clean = story.strip().replace('\n', ' ')
+            if story_clean:  # Ensure the story is not empty
+                # Creating a JSON object for each story
+                json_object = json.dumps({"text": story_clean})
+                file.write(json_object + '\n')
+
+def main():
+    input_file = './data/TinyStories-train.txt'  # Replace with your input file path
+    output_file = './data/TinyStories-train.jsonl'  # Replace with your output file path
+
+    with open(input_file, 'r') as file:
+        text = file.read()
+
+        stories = split_stories(text)
+        convert_to_jsonl(stories, output_file)
+
+    print(f"Converted stories to {output_file}")
+
+if __name__ == "__main__":
+    main()
